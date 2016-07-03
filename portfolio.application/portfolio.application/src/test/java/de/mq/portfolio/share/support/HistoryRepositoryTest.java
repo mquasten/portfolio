e4d30a292961c92033dee6072024718095bd8b3a
Manfred Quasten <manfred.quasten@arcor.de>
2016-07-03 14:16:37 +0200

    coverage

--- portfolio.application/portfolio.application/src/test/java/de/mq/portfolio/share/support/HistoryRepositoryTest.java
+++ portfolio.application/portfolio.application/src/test/java/de/mq/portfolio/share/support/HistoryRepositoryTest.java
@@ -16,8 +16,11 @@ import de.mq.portfolio.share.TimeCourse;
 public class HistoryRepositoryTest {
 	
 	private static final Double DIVIDEND = 1.52;
-	private static final String DATE = "1968-05-28";
-	private static final Integer RATE = 9500;
+	private static final String DATE1 = "1980-11-18";
+	private static final Integer RATE1 = 9500;
+	
+	private static final String DATE2 = "19-05-28";
+	private static final Integer RATE2 = 17500;
 	private static final String CODE = "^IDAXI";
 	private final RestOperations restOperations = Mockito.mock(RestOperations.class);
 	private final Share share = Mockito.mock(Share.class);
@@ -31,9 +34,9 @@ public class HistoryRepositoryTest {
 			Assert.assertEquals(String.class, (i.getArguments()[1]));
 			final String url = (String) i.getArguments()[0];
 			if( url.endsWith("&g=v")) {
-				return String.format("%s,%s\n,xxx,xxx", DATE, DIVIDEND);
+				return String.format("%s,%s\n,xxx,xxx", DATE1, DIVIDEND);
 			} else {
-				return String.format("%s,xxx,xxx,xxx,%s\n ", DATE, RATE); 
+				return String.format("%s,xxx,xxx,xxx,%s\n%s,xxx,xxx,xxx,%s\n  ", DATE1, RATE1, DATE2, RATE2); 
 			}
 			
 		}).when(restOperations).getForObject(Mockito.anyString(), Mockito.any());
@@ -43,16 +46,21 @@ public class HistoryRepositoryTest {
 	@Test
 	public final void history() throws ParseException {
 		final TimeCourse timeCourse = historyRepository.history(share);
-		Assert.assertEquals(1, timeCourse.rates().size());
+		Assert.assertEquals(2, timeCourse.rates().size());
 		Assert.assertEquals(1, timeCourse.dividends().size());
 		Assert.assertTrue(timeCourse.rates().stream().findAny().isPresent());
 		Assert.assertTrue(timeCourse.dividends().stream().findAny().isPresent());
-		final Data rate = timeCourse.rates().stream().findAny().get();
-		Assert.assertEquals(RATE.doubleValue(), rate.value());
-		Assert.assertEquals( ((HistoryRestRepositoryImpl)historyRepository).df.parseObject(DATE), rate.date());
+		final Data rate1= timeCourse.rates().stream().findFirst().get();
+		Assert.assertEquals(RATE1.doubleValue(), rate1.value());
+		Assert.assertEquals( ((HistoryRestRepositoryImpl)historyRepository).df.parseObject(DATE1), rate1.date());
+		
+		final Data rate2= timeCourse.rates().get(1);
+		Assert.assertEquals(RATE2.doubleValue(), rate2.value());
+		Assert.assertEquals( ((HistoryRestRepositoryImpl)historyRepository).df.parseObject(DATE2), rate2.date());
+		
 		
 		final Data dividend = timeCourse.dividends().stream().findAny().get();
 		Assert.assertEquals(DIVIDEND.doubleValue(), dividend.value());
-		Assert.assertEquals( ((HistoryRestRepositoryImpl)historyRepository).df.parseObject(DATE), dividend.date());
+		Assert.assertEquals( ((HistoryRestRepositoryImpl)historyRepository).df.parseObject(DATE1), dividend.date());
 	}
 }
--- portfolio.application/portfolio.application/src/test/java/de/mq/portfolio/share/support/ShareServiceTest.java
+++ portfolio.application/portfolio.application/src/test/java/de/mq/portfolio/share/support/ShareServiceTest.java
@@ -1,12 +1,17 @@
 package de.mq.portfolio.share.support;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Optional;
 
 import junit.framework.Assert;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import de.mq.portfolio.share.Share;
 import de.mq.portfolio.share.ShareService;
@@ -16,6 +21,7 @@ import de.mq.portfolio.share.support.ShareRepository;
 
 public class ShareServiceTest {
 
+	private static final Number PAGE_SIZE = 50;
 	HistoryRepository historyRepository = Mockito.mock(HistoryRepository.class);
 	ShareRepository shareRepository = Mockito.mock(ShareRepository.class);
 
@@ -24,12 +30,38 @@ public class ShareServiceTest {
 	private final Share share = Mockito.mock(Share.class);
 
 	private final TimeCourse timeCourse = Mockito.mock(TimeCourse.class);
+	
+	private final Collection<String> indexes = new ArrayList<>();
+	
+	private final Pageable pageable = Mockito.mock(Pageable.class);
+	
+	private final Collection<TimeCourse> timeCourses = new ArrayList<>();
+	private Sort sort = Mockito.mock(Sort.class);
+	
+	private final static String DAX = "Deutscher Aktien Index"; 
+	
+	private final static String CODE = "^GDAXI"; 
+	
+	
+	@Before
+	public final void setup() {
+		indexes.add(DAX);
+		Mockito.when(shareRepository.distinctIndex()).thenReturn(indexes);
+		timeCourses.add(timeCourse);
+		Mockito.when(shareRepository.timeCourses(pageable, share)).thenReturn(timeCourses);
+		
+		Mockito.when(shareRepository.pageable(share,sort, PAGE_SIZE)).thenReturn(pageable);
+		
+		Mockito.when(shareRepository.timeCourses(Arrays.asList(CODE))).thenReturn(timeCourses);
+		
+	}
 
 	@Test
 	public void timeCourse() {
 		Mockito.when(historyRepository.history(share)).thenReturn(timeCourse);
 
 		Assert.assertEquals(timeCourse, shareService.timeCourse(share));
+		
 	}
 
 	@Test
@@ -55,5 +87,32 @@ public class ShareServiceTest {
 		shareService.save(share);
 		Mockito.verify(shareRepository).save(share);
 	}
+	
+	
+	@Test
+	public void indexes() {
+		Assert.assertEquals(indexes, shareService.indexes());
+		Mockito.verify(shareRepository).distinctIndex();
+	}
+	
+	@Test
+	public void timeCourses() {
+		Assert.assertEquals(timeCourses, shareService.timeCourses(pageable, share));
+		Mockito.verify(shareRepository).timeCourses(pageable, share);
+	}
+	
+	@Test
+	public void pageable() {
+		Assert.assertEquals(pageable, shareService.pageable(share, sort, PAGE_SIZE));
+		Mockito.verify(shareRepository).pageable(share, sort, PAGE_SIZE);
+	}
 
+	
+	@Test
+	public void timeCoursebyCode() {
+		Optional<TimeCourse> result = shareService.timeCourse(CODE);
+		Assert.assertTrue(result.isPresent());
+		Assert.assertEquals(timeCourse, result.get());
+		Mockito.verify(shareRepository, Mockito.times(1)).timeCourses(Arrays.asList(CODE));
+	}
 }
