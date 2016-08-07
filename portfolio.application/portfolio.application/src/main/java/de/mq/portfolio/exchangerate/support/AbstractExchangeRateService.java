c5a7439e6325f16866cd030276452c104e774d13
Manfred Quasten <manfred.quasten@arcor.de>
2016-08-07 14:40:30 +0200

    methodInjection abtract LookupMethod, ExchangeRateService

--- portfolio.application/portfolio.application/src/main/java/de/mq/portfolio/exchangerate/support/AbstractExchangeRateService.java
+++ portfolio.application/portfolio.application/src/main/java/de/mq/portfolio/exchangerate/support/AbstractExchangeRateService.java
@@ -11,52 +11,65 @@ import de.mq.portfolio.exchangerate.ExchangeRateCalculator;
 import de.mq.portfolio.share.Data;
 
 @Service("exchangeRateService")
-class ExchangeRateServiceImpl implements ExchangeRateService {
-	
+abstract class AbstractExchangeRateService implements ExchangeRateService {
+
 	private final ExchangeRateDatebaseRepository exchangeRateDatebaseRepository;
-	
+
 	private final ExchangeRateRepository exchangeRateRepository;
-	
+
 	@Autowired
-	public ExchangeRateServiceImpl(final ExchangeRateDatebaseRepository exchangeRateDatebaseRepository, final ExchangeRateRepository exchangeRateRepository) {
+	AbstractExchangeRateService(final ExchangeRateDatebaseRepository exchangeRateDatebaseRepository, final ExchangeRateRepository exchangeRateRepository) {
 		this.exchangeRateDatebaseRepository = exchangeRateDatebaseRepository;
-		this.exchangeRateRepository=exchangeRateRepository;
+		this.exchangeRateRepository = exchangeRateRepository;
 	}
-	
-	/* (non-Javadoc)
-	 * @see de.mq.portfolio.exchangerate.support.ExchangeRateService#exchangeRate(de.mq.portfolio.exchangerate.ExchangeRate)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * de.mq.portfolio.exchangerate.support.ExchangeRateService#exchangeRate(de.
+	 * mq.portfolio.exchangerate.ExchangeRate)
 	 */
 	@Override
-	public final ExchangeRate exchangeRate(final ExchangeRate exchangeRate){
+	public final ExchangeRate exchangeRate(final ExchangeRate exchangeRate) {
 		final Collection<Data> rates = exchangeRateRepository.history(exchangeRate.link());
 		exchangeRate.assign(rates);
 		return exchangeRate;
 	}
 
-	/* (non-Javadoc)
-	 * @see de.mq.portfolio.exchangerate.support.ExchangeRateService#save(de.mq.portfolio.exchangerate.ExchangeRate)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see de.mq.portfolio.exchangerate.support.ExchangeRateService#save(de.mq.
+	 * portfolio.exchangerate.ExchangeRate)
 	 */
 	@Override
 	public final void save(final ExchangeRate exchangeRate) {
 		exchangeRateDatebaseRepository.save(exchangeRate);
 	}
-	
-	/* (non-Javadoc)
-	 * @see de.mq.portfolio.exchangerate.support.ExchangeRateService#exchangeRateCalculator(java.util.Collection)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see de.mq.portfolio.exchangerate.support.ExchangeRateService#
+	 * exchangeRateCalculator(java.util.Collection)
 	 */
 	@Override
 	public final ExchangeRateCalculator exchangeRateCalculator(final Collection<ExchangeRate> exchangerates) {
 		return newBuilder().withExchangeRates(exchangeRateDatebaseRepository.exchangerates(exchangerates)).build();
 	}
-	
-	/* (non-Javadoc)
-	 * @see de.mq.portfolio.exchangerate.support.ExchangeRateService#exchangeRateCalculator()
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see de.mq.portfolio.exchangerate.support.ExchangeRateService#
+	 * exchangeRateCalculator()
 	 */
 	@Override
 	public final ExchangeRateCalculator exchangeRateCalculator() {
 		return newBuilder().withExchangeRates(exchangeRateDatebaseRepository.exchangerates()).build();
 	}
-	
+
 	@Lookup
-	ExchangeRateCalculatorBuilder newBuilder() {return null;};
+	abstract ExchangeRateCalculatorBuilder newBuilder();
 }
--- portfolio.application/portfolio.application/src/test/java/de/mq/portfolio/exchangerate/support/ExchangeRateServiceTest.java
+++ portfolio.application/portfolio.application/src/test/java/de/mq/portfolio/exchangerate/support/ExchangeRateServiceTest.java
@@ -2,38 +2,55 @@ package de.mq.portfolio.exchangerate.support;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
+import org.springframework.beans.BeanUtils;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.util.ReflectionUtils;
+
 import de.mq.portfolio.exchangerate.ExchangeRate;
 import de.mq.portfolio.exchangerate.ExchangeRateCalculator;
 import de.mq.portfolio.share.Data;
 import junit.framework.Assert;
 
+
 public class ExchangeRateServiceTest {
 
 	
 	private static final String LINK = "http://www.bundesbank.de/cae/servlet/StatisticDownload?tsId=BBEX3.D.USD.EUR.BB.AC.000&its_csvFormat=de&its_fileFormat=csv&mode=its";
-	private final ExchangeRateDatebaseRepository exchangeRateDatebaseRepository = Mockito.mock(ExchangeRateDatebaseRepository.class); 
-	private final ExchangeRateRepository exchangeRateRepository = Mockito.mock(ExchangeRateRepository.class);
-	private final ExchangeRateService exchangeRateService = Mockito.spy(new ExchangeRateServiceImpl(exchangeRateDatebaseRepository, exchangeRateRepository));
+	private  final ExchangeRateDatebaseRepository exchangeRateDatebaseRepository = Mockito.mock(ExchangeRateDatebaseRepository.class); 
+	private final  ExchangeRateRepository exchangeRateRepository = Mockito.mock(ExchangeRateRepository.class);  ;
+	private  final ExchangeRateService exchangeRateService = Mockito.mock(AbstractExchangeRateService.class , Mockito.CALLS_REAL_METHODS );
 	private final  ExchangeRateCalculatorBuilder builder = Mockito.mock(ExchangeRateCalculatorBuilder.class);
 	private final ExchangeRate exchangeRate = Mockito.mock(ExchangeRate.class);
 	private final Data rows = Mockito.mock(Data.class);
 	private final Collection<Data> rates = Arrays.asList(rows);
 	private final Collection<ExchangeRate> exchangeRates = Arrays.asList(exchangeRate);
-	
 	private final ExchangeRateCalculator  exchangeRateCalculator = Mockito.mock(ExchangeRateCalculator.class);
+	
+	private final Map<Class<?> ,Object> dependencies = new HashMap<>();
+	
 	@Before
-	public final void setup() {
-		Mockito.when(((ExchangeRateServiceImpl)exchangeRateService).newBuilder()).thenReturn(builder);
+	public final void setup() throws NoSuchMethodException, SecurityException {
+		
+		Mockito.when(builder.withExchangeRates(exchangeRates)).thenReturn(builder);
+		Mockito.when(builder.build()).thenReturn(exchangeRateCalculator);
+		
+		dependencies.clear();
+		dependencies.put(ExchangeRateRepository.class, exchangeRateRepository);
+		dependencies.put(ExchangeRateDatebaseRepository.class, exchangeRateDatebaseRepository);
+		ReflectionUtils.doWithFields(exchangeRateService.getClass(), field -> ReflectionTestUtils.setField(exchangeRateService, field.getName(), dependencies.get(field.getType())), field -> dependencies.containsKey(field.getType()));
+		
+		Mockito.doReturn(builder).when(((AbstractExchangeRateService)exchangeRateService)).newBuilder();
 	}
 	
 	@Test
 	public final void exchangeRate() {
 		Mockito.when(exchangeRate.link()).thenReturn(LINK);
-		
 		Mockito.when(exchangeRateRepository.history(exchangeRate.link())).thenReturn(rates);
 		
 		Assert.assertEquals(exchangeRate, exchangeRateService.exchangeRate(exchangeRate));
@@ -49,27 +66,35 @@ public class ExchangeRateServiceTest {
 	@Test
 	public final void exchangeRateCalculator() {
 		Mockito.when(exchangeRateDatebaseRepository.exchangerates()).thenReturn(exchangeRates);
-		Mockito.when(builder.withExchangeRates(exchangeRates)).thenReturn(builder);
-		Mockito.when(builder.build()).thenReturn(exchangeRateCalculator);
+		
 		Assert.assertEquals(exchangeRateCalculator, exchangeRateService.exchangeRateCalculator());
 		
 		Mockito.verify(builder).build();
 		Mockito.verify(builder).withExchangeRates(exchangeRates);
-		Mockito.verify(exchangeRateDatebaseRepository).exchangerates();
+		Mockito.verify(exchangeRateDatebaseRepository).exchangerates(); 
 		
 	}
 	
 	@Test
 	public final void exchangeRateCalculatorAsArgument() {
-		Mockito.when(builder.withExchangeRates(exchangeRates)).thenReturn(builder);
-		Mockito.when(builder.build()).thenReturn(exchangeRateCalculator);
 		Mockito.when(exchangeRateDatebaseRepository.exchangerates(exchangeRates)).thenReturn(exchangeRates);
+		
 		Assert.assertEquals(exchangeRateCalculator, exchangeRateService.exchangeRateCalculator(exchangeRates));
 		
 		Mockito.verify(builder).build();
 		Mockito.verify(builder).withExchangeRates(exchangeRates);
 		Mockito.verify(exchangeRateDatebaseRepository).exchangerates(exchangeRates);
 		
+	}
+	
+	@Test
+	public final void constructorInjection() throws NoSuchMethodException, SecurityException {
+		final ExchangeRateService service  = BeanUtils.instantiateClass( exchangeRateService.getClass().getDeclaredConstructor(ExchangeRateDatebaseRepository.class, ExchangeRateRepository.class),exchangeRateDatebaseRepository , exchangeRateRepository);
+		final Map<Class<?> ,Object> results = new HashMap<>();
+		ReflectionUtils.doWithFields(service.getClass(), field -> results.put(field.getType(), ReflectionTestUtils.getField(service, field.getName())), field -> dependencies.containsKey(field.getType()));
+	    Assert.assertEquals(2, results.size());
+	    Assert.assertEquals(exchangeRateRepository, results.get(ExchangeRateRepository.class));
+	    Assert.assertEquals(exchangeRateDatebaseRepository, results.get(ExchangeRateDatebaseRepository.class));
 	}
 	
 }
