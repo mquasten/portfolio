537dc012283c44c5f30ee1d1f2cbe3048781e5ed
Manfred Quasten <manfred.quasten@e-integration.de>
2016-08-18 16:39:59 +0200

    CommandLineProcessor

--- portfolio.batch/portfilio.batch/src/main/java/de/mq/portfolio/support/SimpleCommandlineProcessorImpl.java
+++ portfolio.batch/portfilio.batch/src/main/java/de/mq/portfolio/support/SimpleCommandlineProcessorImpl.java
@@ -9,19 +9,27 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.function.Function;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.util.Assert;
 import org.springframework.util.ReflectionUtils;
 
 
-public  class SimpleCommandlineProcessorImpl  implements BeanFactoryPostProcessor{
+public  class SimpleCommandlineProcessorImpl implements BeanFactoryPostProcessor {
 	
 	private static Method method;
 	private static Object target;
+	
+	private static final String [] packages= new String [] {SimpleCommandlineProcessorImpl.class.getPackage().getName()};
+	
+	
+	private final static Function<String[], ApplicationContext> applicationContextSupplier =  packages -> new AnnotationConfigApplicationContext(packages) ;
 	
 	@Retention(RetentionPolicy.RUNTIME)
 	@interface Main {
@@ -62,8 +70,13 @@ public  class SimpleCommandlineProcessorImpl  implements BeanFactoryPostProcesso
 	
 	public static final void main(final String[] args) {
 		
+		
+		applicationContextSupplier.apply(packages);
+		
+		
 		Assert.notNull(target, "TargetBean is required, possibly BeanFactoryPostProcessor is not registered.");
 		Assert.notNull(method, "Method is required, possibly BeanFactoryPostProcessor is not registered.");
+		
 		if( method.getParameterTypes().length==0) {
 			ReflectionUtils.invokeMethod(method, target);
 			return;
@@ -78,9 +91,14 @@ public  class SimpleCommandlineProcessorImpl  implements BeanFactoryPostProcesso
 			ReflectionUtils.invokeMethod(method, target, new HashSet<>(Arrays.asList(args)));
 			return;
 		}
-		
+
 		ReflectionUtils.invokeMethod(method, target, Arrays.asList(args));
 		
 	}
+
+
+
+	
+	
 
 }
--- portfolio.batch/portfilio.batch/src/test/java/de/mq/portfolio/support/CommandlineProcessorIntegrationTest.java
+++ portfolio.batch/portfilio.batch/src/test/java/de/mq/portfolio/support/CommandlineProcessorIntegrationTest.java
@@ -1,22 +1,60 @@
 package de.mq.portfolio.support;
 
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.util.ReflectionUtils;
 
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes={RulesConfiguration.class})
+
+
+
 public class CommandlineProcessorIntegrationTest {
 
+	private static final String[] ARGS = new String[] {"Kylie", "is" , "nice"};
+	private final Map<Class<?>, Field> fields = new HashMap<>();
+	private TestBean testBean = Mockito.mock(TestBean.class);
+	private Method method = ReflectionUtils.findMethod(testBean.getClass(), "process", Collection.class);
+	
+
+	
+	@Before
+	public final void setup() throws NoSuchMethodException, SecurityException {
+		ReflectionUtils.doWithFields(SimpleCommandlineProcessorImpl.class, field -> {field.setAccessible(true);fields.put(field.getType(), field);}, field -> Modifier.isStatic(field.getModifiers()));
+		final String[] packages = (String[]) ReflectionUtils.getField(fields.get(String[].class),null);
+		IntStream.range(0, packages.length).forEach(i -> packages[i]=" ");
+		
+		ReflectionUtils.setField(fields.get(Method.class), null, method);
+		ReflectionUtils.setField(fields.get(Object.class), null, testBean);
+	}
 	
 	@Test
 	public final void main()  {
 		
-		SimpleCommandlineProcessorImpl.main(new String[] {"Kylie", "is" , "nice"});
+		SimpleCommandlineProcessorImpl.main(ARGS);
+		
+		Mockito.verify(testBean).process(Arrays.asList(ARGS));
 		
 	}
+	
+}
+
+
+
+
+ interface  TestBean {
+	
+	void process(Collection<String> args);
+	
 	
 }
