009eeefa229583cd7c64945e5f8e528e5eec4ff9
Manfred Quasten <manfred.quasten@arcor.de>
2016-11-22 20:46:18 +0100

    SerialisationUtil

--- portfolio.web/src/main/java/de/mq/portfolio/support/SimpleSerialisationUtilImpl.java
+++ portfolio.web/src/main/java/de/mq/portfolio/support/SimpleSerialisationUtilImpl.java
@@ -1,12 +1,37 @@
 package de.mq.portfolio.support;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.Arrays;
+import java.util.Base64;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
+
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.client.ResourceAccessException;
 
 public class SimpleSerialisationUtilImpl {
 	
-	public final Map<String,Object> serialize(final Object  bean, final Collection<?>   properties) {
-		return null;
+	final ExceptionTranslationBuilder<String, ByteArrayOutputStream> builder = new ExceptionTranslationBuilderImpl<>();
+	
+	public final Map<String,Object> toMap(final Object  bean, final Collection<?>   properties) {
+		final Map<String,Object> results = new HashMap<>();
+		ReflectionUtils.doWithFields(bean.getClass(), field -> {field.setAccessible(true); results.put(field.getName(), ReflectionUtils.getField(field, bean));}, field -> properties.contains(field.getName()));
+		return results;
+		
+	}
+	
+	public final String serialize(final Map<String,Object> values)  {
+	
+		return builder.withResource(() -> new ByteArrayOutputStream()).withTranslation(ResourceAccessException.class, Arrays.asList(IOException.class)).withStatement((os) -> {
+			final ObjectOutputStream oos = new ObjectOutputStream(os);
+			oos.writeObject(values);
+			return Base64.getEncoder().encodeToString(os.toByteArray());
+		}).translate();
+		
+		
 		
 	}
 
