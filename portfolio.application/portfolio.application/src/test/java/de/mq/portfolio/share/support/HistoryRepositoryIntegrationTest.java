aa379273cae6bde8e244d1af027c92870ca0dd77
Manfred Quasten <manfred.quasten@arcor.de>
2017-02-21 21:10:00 +0100

    DB11 test compare google + yahoo

--- portfolio.application/portfolio.application/src/test/java/de/mq/portfolio/share/support/HistoryRepositoryIntegrationTest.java
+++ portfolio.application/portfolio.application/src/test/java/de/mq/portfolio/share/support/HistoryRepositoryIntegrationTest.java
@@ -102,7 +102,7 @@ public class HistoryRepositoryIntegrationTest {
 		int missing[] = new int[] {0};
 		dates.forEach(date -> {
 			    final Double values [] = prices.get(date);
-			    System.out.println(df.format(date) +";" +  values[0] +";" +values[1] +";" +values[2] +";" +values[3]);
+			   // System.out.println(df.format(date) +";" +  values[0] +";" +values[1] +";" +values[2] +";" +values[3]);
 			    
 			   if(  Arrays.asList(values).stream().filter(value -> value == null).count() > 0 ) {
 				   missing[0]++;
@@ -232,6 +232,63 @@ public class HistoryRepositoryIntegrationTest {
 			    
 				
 		});
+		
+		Assert.assertTrue(counter[0]> 110);
+	  	Assert.assertTrue( missing[0] < 5  );
+	}
+	
+	
+	@Test
+	public  void historyDB11()   {
+	
+		Mockito.when(share.code()).thenReturn("DB11.DE");
+		Mockito.when(share.code2()).thenReturn("ETR:DB11");
+		
+		int max = 2;
+		@SuppressWarnings("unchecked")
+		final List<Data>[]  results = new  List[max];
+				
+		
+		results[0] = historyGoogleRestRepository.history(share).rates();
+		results[1] = historyYahooRestRepository.history(share).rates().stream().filter(rate -> ! rate.date().before(new GregorianCalendar(2016, 8 , 5).getTime() )).collect(Collectors.toList());
+		
+		final Map<Date, Double[]> prices = new HashMap<>();
+		IntStream.range(0, max).forEach(i -> {
+			results[i].forEach(data -> {
+				prices.put(data.date(), new Double[max]);
+			});
+			
+		});
+		
+		
+		IntStream.range(0, max).forEach(i ->  {
+			results[i].forEach(data -> prices.get(data.date())[i]=data.value());
+			
+		});
+		
+		final List<Date> dates = new ArrayList<>(prices.keySet());
+		Collections.sort(dates, (d1, d2) -> (int) Math.round(d1.getTime() - d2.getTime()));
+		
+		final int missing[] = new int[] {0};
+		final int counter[] =  new int[] {0};
+		dates.stream().filter(date ->  ! date.before(new GregorianCalendar(2016, 8 , 12).getTime())).forEach(date -> {
+			    final Double values [] = prices.get(date);
+			    //System.out.println(df.format(date) +";" +  values[0] +";" +values[1] );
+			    
+			    
+			    counter[0]++;
+			    
+			    if(  values[0] != null &&  values[1] != null  )  {
+			    	Assert.assertTrue(Math.abs(Math.round(100d*values[0]) - Math.round(100d*values[1])) <= 1d );
+			    	
+			    } else {
+			    	missing[0]++;
+			    }
+			  
+			    
+				
+		});
+		
 		
 		Assert.assertTrue(counter[0]> 110);
 	  	Assert.assertTrue( missing[0] < 5  );
