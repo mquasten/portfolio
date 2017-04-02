60d7b98e13451f7a4efc34bcd2316d4c3aefa91c
Manfred Quasten <manfred.quasten@arcor.de>
2017-04-02 15:28:45 +0200

    realtime courses portfolio

--- portfolio.web/src/main/java/de/mq/portfolio/shareportfolio/support/RealtimeCoursesAO.java
+++ portfolio.web/src/main/java/de/mq/portfolio/shareportfolio/support/RealtimeCoursesAO.java
@@ -1,19 +1,45 @@
 package de.mq.portfolio.shareportfolio.support;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import de.mq.portfolio.share.Data;
+import de.mq.portfolio.share.TimeCourse;
+
 @Component("realtimeCourses")
 @Scope("view")
-public class RealtimeCoursesAO implements Serializable {
+public class RealtimeCoursesAO  implements Serializable {
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	private Collection<Map<String,Object>> shares= new ArrayList<>(); 
 
+	
+	private final List<String> shareColumns = Arrays.asList("name", "delta", "deltaPercent");
+	
+
+	public Collection<String> getShareColumns() {
+		return shareColumns;
+	}
+
+	public Collection<Map<String, Object>> getShares() {
+		return shares;
+	}
+
+	
 	private String portfolioId;
 	
 	private String filter;
@@ -33,5 +59,20 @@ public class RealtimeCoursesAO implements Serializable {
 	public void setPortfolioId(String portfolioId) {
 		this.portfolioId = portfolioId;
 	}
+	
+	void assign(final List<Entry<TimeCourse,List<Data>>> entries) {
+		shares.clear();
+		shares.addAll(entries.stream().map(entry ->  shareEntryToMap(entry)).collect(Collectors.toList()));
+	}
+
+	private Map<String, Object> shareEntryToMap(final Entry<TimeCourse, List<Data>> entry) {
+		final Map<String,Object> values = new HashMap<>();
+		values.put(shareColumns.get(0), entry.getKey().name() +" (" + entry.getKey().code() +")");
+		values.put(shareColumns.get(1),   entry.getValue().get(1).value() - (Double) entry.getValue().get(0).value() );
+		values.put(shareColumns.get(2),   (entry.getValue().get(1).value() - (Double) entry.getValue().get(0).value()) / entry.getValue().get(0).value() );
+		return values;
+	}
+	
+
 
 }
--- portfolio.web/src/main/java/de/mq/portfolio/shareportfolio/support/RealtimeCoursesController.java
+++ portfolio.web/src/main/java/de/mq/portfolio/shareportfolio/support/RealtimeCoursesController.java
@@ -1,17 +1,13 @@
 package de.mq.portfolio.shareportfolio.support;
 
 import java.util.AbstractMap;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import de.mq.portfolio.share.Data;
 import de.mq.portfolio.share.ShareService;
 import de.mq.portfolio.share.TimeCourse;
 import de.mq.portfolio.shareportfolio.SharePortfolio;
@@ -22,7 +18,6 @@ public class RealtimeCoursesController {
 	private final SharePortfolioService sharePortfolioService;
 	private final ShareService shareService;
 	
-	private final List<Entry<TimeCourse, List<Data>>> timeCourses = new ArrayList<>(); ; 
 	
 
 
@@ -40,19 +35,22 @@ public class RealtimeCoursesController {
 	
 	
 	public void init(RealtimeCoursesAO realtimeCourses) {
+		
+		System.out.println(realtimeCourses);
+		
+		System.out.println(realtimeCourses.getPortfolioId());
+	
 		final SharePortfolio sharePortfolio = sharePortfolioService.sharePortfolio(realtimeCourses.getPortfolioId());
 		final Map<String, TimeCourse> timeCoursesMap = new HashMap<>();
 		
 		sharePortfolio.timeCourses().stream().forEach(tc -> timeCoursesMap.put(tc.code(),tc));
-		timeCourses.clear();
 		
-		timeCourses.addAll(shareService.realTimeCourses(timeCoursesMap.keySet()).stream().map(tc -> new AbstractMap.SimpleImmutableEntry<>(timeCoursesMap.get(tc.code()), tc.rates())).collect(Collectors.toList()));
 		
+		realtimeCourses.assign(shareService.realTimeCourses(timeCoursesMap.keySet()).stream().map(tc -> new AbstractMap.SimpleImmutableEntry<>(timeCoursesMap.get(tc.code()), tc.rates())).collect(Collectors.toList()));
+	
 		
 	}
 
 	
-	public List<Entry<TimeCourse, List<Data>>> getTimeCourses() {
-		return timeCourses;
-	}
+	
 }
