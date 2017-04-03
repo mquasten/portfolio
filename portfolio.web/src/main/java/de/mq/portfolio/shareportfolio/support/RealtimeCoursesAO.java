b66eca9f5c65a97269ee9d0180e58e647af47cc3
Manfred Quasten <manfred.quasten@arcor.de>
2017-04-03 21:20:14 +0200

    factorMap realtime portfolio

--- portfolio.web/src/main/java/de/mq/portfolio/shareportfolio/support/RealtimeCoursesAO.java
+++ portfolio.web/src/main/java/de/mq/portfolio/shareportfolio/support/RealtimeCoursesAO.java
@@ -25,7 +25,9 @@ public class RealtimeCoursesAO  implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	private Collection<Map<String,Object>> shares= new ArrayList<>(); 
+	private final Collection<Map<String,Object>> shares= new ArrayList<>(); 
+	
+	 private final Map<String, Double> factors = new HashMap<>();
 
 	
 	private final List<String> shareColumns = Arrays.asList("name", "delta", "deltaPercent");
@@ -63,6 +65,11 @@ public class RealtimeCoursesAO  implements Serializable {
 	void assign(final List<Entry<TimeCourse,List<Data>>> entries) {
 		shares.clear();
 		shares.addAll(entries.stream().map(entry ->  shareEntryToMap(entry)).collect(Collectors.toList()));
+	}
+	
+	void setFactors(final Map<String, Double> factors ) {
+		this.factors.clear();
+		this.factors.putAll(factors);
 	}
 
 	private Map<String, Object> shareEntryToMap(final Entry<TimeCourse, List<Data>> entry) {
--- portfolio.web/src/main/java/de/mq/portfolio/shareportfolio/support/RealtimeCoursesController.java
+++ portfolio.web/src/main/java/de/mq/portfolio/shareportfolio/support/RealtimeCoursesController.java
@@ -8,6 +8,8 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import de.mq.portfolio.exchangerate.ExchangeRateCalculator;
+import de.mq.portfolio.exchangerate.support.ExchangeRateService;
 import de.mq.portfolio.share.ShareService;
 import de.mq.portfolio.share.TimeCourse;
 import de.mq.portfolio.shareportfolio.SharePortfolio;
@@ -18,37 +20,48 @@ public class RealtimeCoursesController {
 	private final SharePortfolioService sharePortfolioService;
 	private final ShareService shareService;
 	
-	
+	private final ExchangeRateService exchangeRateService;
 
 
 
 
 
 	@Autowired
-	public RealtimeCoursesController(SharePortfolioService sharePortfolioService, ShareService shareService) {
+	public RealtimeCoursesController(final SharePortfolioService sharePortfolioService, final ShareService shareService, final ExchangeRateService exchangeRateService) {
 		this.sharePortfolioService = sharePortfolioService;
 		this.shareService = shareService;
+		this.exchangeRateService =  exchangeRateService;
 	}
 
 
 	
 	
 	
-	public void init(RealtimeCoursesAO realtimeCourses) {
-		
-		System.out.println(realtimeCourses);
-		
-		System.out.println(realtimeCourses.getPortfolioId());
+	public void init(final RealtimeCoursesAO realtimeCourses) {
 	
 		final SharePortfolio sharePortfolio = sharePortfolioService.sharePortfolio(realtimeCourses.getPortfolioId());
+		
+		realtimeCourses.setFactors(factors(sharePortfolio));
 		final Map<String, TimeCourse> timeCoursesMap = new HashMap<>();
 		
 		sharePortfolio.timeCourses().stream().forEach(tc -> timeCoursesMap.put(tc.code(),tc));
-		
-		
+			
 		realtimeCourses.assign(shareService.realTimeCourses(timeCoursesMap.keySet()).stream().map(tc -> new AbstractMap.SimpleImmutableEntry<>(timeCoursesMap.get(tc.code()), tc.rates())).collect(Collectors.toList()));
 	
 		
+	}
+
+
+
+
+
+	private final Map<String, Double>  factors(final SharePortfolio sharePortfolio ) {
+		final Map<String, Double> factors = new HashMap<>();
+		final ExchangeRateCalculator exchangeRateCalculator = exchangeRateService.exchangeRateCalculator(sharePortfolio.exchangeRateTranslations());
+		final Map<TimeCourse,Double> weights = sharePortfolio.min();
+		
+		sharePortfolio.timeCourses().forEach(tc -> factors.put(tc.code(), exchangeRateCalculator.factor(sharePortfolio.exchangeRate(tc), tc.end()) * weights.get(tc)));
+	    return factors;
 	}
 
 	
