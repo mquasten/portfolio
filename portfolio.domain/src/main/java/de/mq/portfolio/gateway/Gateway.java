563e28f62a078c11f2334b8788ba966d01d0cd24
Manfred Quasten <manfred.quasten@arcor.de>
2017-07-01 18:53:41 +0200

    Gateway coverage

--- portfolio.domain/src/main/java/de/mq/portfolio/gateway/Gateway.java
+++ portfolio.domain/src/main/java/de/mq/portfolio/gateway/Gateway.java
@@ -8,45 +8,52 @@ import org.springframework.util.Assert;
 import org.springframework.util.StringUtils;
 
 public enum Gateway {
-	
-	ArivaRateHistory("ARH"),
-	CentralBankExchangeRates("BER");
-	
+
+	ArivaRateHistory("ARH"), CentralBankExchangeRates("BER");
+
 	static final String DELIMITER = "-";
 	private final String id;
+
 	Gateway(final String id) {
-		this.id=id;
+		this.id = id;
 	}
-	
+
 	String id() {
 		return id;
 	}
-	
-	public String id(final String ... keys ) {
+
+	public String id(final String... keys) {
 		Assert.notNull(keys, "Keys is mandatory.");
 		Assert.notEmpty(Arrays.asList(keys), "At lets one key expected.");
 		return StringUtils.arrayToDelimitedString(keys, DELIMITER) + DELIMITER + id;
-
 	}
-	
-	public static String code(final String id){
+
+	public static String code(final String id) {
 		final int index = id.lastIndexOf(DELIMITER);
 		idSyntaxGuard(id, index);
 		return id.substring(0, index);
 	}
 
 	private static void idSyntaxGuard(final String id, final int index) {
-		Assert.hasText(id);
-		Assert.isTrue(index > 0, "Invalid id, must contain: " + DELIMITER);
-		Assert.isTrue(id.length() > index+1 , "Invalid id, must contains gatewayId at the end.");
+		Assert.hasText(id, "Id is mandatory.");
+		Assert.isTrue(index > 0, "Invalid id, must contain: " + DELIMITER + ".");
+		Assert.isTrue(id.length() > index + 1, "Invalid id, must contains gatewayId at the end.");
 	}
-	
-	public static Gateway gateway(final String id){
+
+	public static Gateway gateway(final String id) {
+		Assert.hasText(id, "Id is mandatory.");
 		final int index = id.lastIndexOf(DELIMITER);
+		if ((index < 0)) {
+			return gatewayValue(id);
+		}
+
 		idSyntaxGuard(id, index);
-		final String gatewayId =  id.substring(index+1, id.length());
+		final String gatewayId = id.substring(index + 1, id.length());
+		return gatewayValue(gatewayId);
+	}
+
+	private static Gateway gatewayValue(final String gatewayId) {
 		return DataAccessUtils.requiredSingleResult(Arrays.asList(values()).stream().filter(value -> value.id().equals(gatewayId)).collect(Collectors.toSet()));
 	}
-	
 
 }
--- portfolio.domain/src/test/java/de/mq/portfolio/gateway/GatewayTest.java
+++ portfolio.domain/src/test/java/de/mq/portfolio/gateway/GatewayTest.java
@@ -1,31 +1,66 @@
 package de.mq.portfolio.gateway;
 
+import java.util.Arrays;
+
 import org.junit.Assert;
 import org.junit.Test;
 
-
-
 public class GatewayTest {
-	
+
 	private static final String ID_PATTERN = "%s-%s";
 	private static String CODE = "SAP.DE";
-	
-	
-	private static String EXCHANGE_RATE  = "EUR-USD";
-	
+
+	private static String EXCHANGE_RATE = "EUR-USD";
+
 	@Test
 	public final void code() {
 		Assert.assertEquals(CODE, Gateway.code(String.format(ID_PATTERN, CODE, Gateway.ArivaRateHistory.id())));
-		
+
 		Assert.assertEquals(EXCHANGE_RATE, Gateway.code(String.format(ID_PATTERN, EXCHANGE_RATE, Gateway.CentralBankExchangeRates.id())));
 	}
-	
-	
+
 	@Test
 	public final void gateway() {
-		Assert.assertEquals(Gateway.ArivaRateHistory, Gateway.gateway(String.format(ID_PATTERN, CODE,Gateway.ArivaRateHistory.id() )));
-		
-		Assert.assertEquals(Gateway.CentralBankExchangeRates, Gateway.gateway(String.format(ID_PATTERN, EXCHANGE_RATE, Gateway.CentralBankExchangeRates.id() )));
+		Assert.assertEquals(Gateway.ArivaRateHistory, Gateway.gateway(String.format(ID_PATTERN, CODE, Gateway.ArivaRateHistory.id())));
+
+		Assert.assertEquals(Gateway.CentralBankExchangeRates, Gateway.gateway(String.format(ID_PATTERN, EXCHANGE_RATE, Gateway.CentralBankExchangeRates.id())));
 	}
 
+	@Test
+	public final void idWithKey() {
+		Assert.assertEquals(String.format(ID_PATTERN, CODE, Gateway.ArivaRateHistory.id()), Gateway.ArivaRateHistory.id(CODE));
+
+		Assert.assertEquals(String.format(ID_PATTERN, EXCHANGE_RATE, Gateway.ArivaRateHistory.id()), Gateway.ArivaRateHistory.id(EXCHANGE_RATE.split(Gateway.DELIMITER)));
+	}
+
+	@Test
+	public final void gatewayOnly() {
+		Assert.assertEquals(Gateway.ArivaRateHistory, Gateway.gateway(Gateway.ArivaRateHistory.id()));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public final void gatewayInvalid() {
+		try {
+			Gateway.gateway(String.format(ID_PATTERN, CODE, ""));
+		} catch (final IllegalArgumentException ia) {
+			Assert.assertTrue(ia.getMessage().endsWith("gatewayId at the end."));
+			throw ia;
+		}
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public final void wrongcode() {
+		try {
+			Gateway.code(Gateway.ArivaRateHistory.id());
+		} catch (IllegalArgumentException ia) {
+			Assert.assertTrue(ia.getMessage().endsWith("must contain: " + Gateway.DELIMITER + "."));
+			throw ia;
+		}
+
+	}
+
+	@Test
+	public final void createValues() {
+		Arrays.asList(Gateway.values()).forEach(value -> Assert.assertEquals(value, Gateway.valueOf(value.name())));
+	}
 }
