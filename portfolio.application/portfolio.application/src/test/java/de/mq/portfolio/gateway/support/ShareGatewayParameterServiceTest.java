e5c7af8b8cf72b0632d7a30ef38bb1229af864e7
Manfred Quasten <manfred.quasten@arcor.de>
2017-07-24 16:50:56 +0200

    Testcoverage gateway.support

--- portfolio.application/portfolio.application/src/test/java/de/mq/portfolio/gateway/support/ShareGatewayParameterServiceTest.java
+++ portfolio.application/portfolio.application/src/test/java/de/mq/portfolio/gateway/support/ShareGatewayParameterServiceTest.java
@@ -22,72 +22,73 @@ import de.mq.portfolio.gateway.GatewayParameterAggregation;
 import de.mq.portfolio.share.Share;
 
 public class ShareGatewayParameterServiceTest {
-	
+
 	private static final String CODE = "JNJ";
 
 	private final GatewayParameterRepository gatewayParameterRepository = Mockito.mock(GatewayParameterRepository.class);
-	
-	private final ShareGatewayParameterServiceImpl shareGatewayParameterService= Mockito.mock(ShareGatewayParameterServiceImpl.class, Mockito.CALLS_REAL_METHODS);
+
+	private final ShareGatewayParameterServiceImpl shareGatewayParameterService = Mockito.mock(ShareGatewayParameterServiceImpl.class, Mockito.CALLS_REAL_METHODS);
 
 	private final Share share = Mockito.mock(Share.class);
-	
-	private final  GatewayParameter gatewayParameter = Mockito.mock(GatewayParameter.class);
-	private final  Map<Class<?>, Object> dependencies = new HashMap<>();
-	
+
+	private final GatewayParameter gatewayParameter = Mockito.mock(GatewayParameter.class);
+	private final Map<Class<?>, Object> dependencies = new HashMap<>();
+
 	@Before
 	public final void setup() {
-		
+
 		dependencies.put(GatewayParameterRepository.class, gatewayParameterRepository);
 		Arrays.asList(ShareGatewayParameterServiceImpl.class.getDeclaredFields()).stream().filter(field -> dependencies.containsKey(field.getType())).forEach(field -> ReflectionTestUtils.setField(shareGatewayParameterService, field.getName(), dependencies.get(field.getType())));
-	
+
 		Mockito.when(gatewayParameter.gateway()).thenReturn(Gateway.GoogleRateHistory);
 
 		Mockito.when(share.code()).thenReturn(CODE);
-	    Mockito.when(gatewayParameterRepository.gatewayParameter(Gateway.GoogleRateHistory, CODE)).thenReturn(gatewayParameter); 
-	    Mockito.doAnswer(answer-> {
-	    	return new GatewayParameterAggregationBuilderImpl<>();
-	    }).when(shareGatewayParameterService).gatewayParameterAggregationBuilder();
-	    
-	    Mockito.when(gatewayParameterRepository.gatewayParameters(CODE)).thenReturn(Arrays.asList(gatewayParameter));
+		Mockito.when(gatewayParameterRepository.gatewayParameter(Gateway.GoogleRateHistory, CODE)).thenReturn(gatewayParameter);
+		Mockito.doAnswer(answer -> {
+			return new GatewayParameterAggregationBuilderImpl<>();
+		}).when(shareGatewayParameterService).gatewayParameterAggregationBuilder();
+
+		Mockito.when(gatewayParameterRepository.gatewayParameters(CODE)).thenReturn(Arrays.asList(gatewayParameter));
 	}
+
 	@Test
 	public final void gatewayParameter() {
 		final GatewayParameterAggregation<Share> gatewayParameterAggregation = shareGatewayParameterService.gatewayParameter(share, Arrays.asList(Gateway.GoogleRateHistory));
-	
+
 		Assert.assertEquals(share, gatewayParameterAggregation.domain());
 		Assert.assertEquals(1, gatewayParameterAggregation.gatewayParameters().size());
-		Assert.assertEquals(gatewayParameter,  gatewayParameterAggregation.gatewayParameters().stream().findAny().get());
-	   
+		Assert.assertEquals(gatewayParameter, gatewayParameterAggregation.gatewayParameters().stream().findAny().get());
+
 	}
-	
-	@Test(expected=IllegalArgumentException.class)
+
+	@Test(expected = IllegalArgumentException.class)
 	public final void gatewayParameterGatewaysEmpty() {
 		shareGatewayParameterService.gatewayParameter(share, Arrays.asList());
-	    
-	
+
 	}
-	
+
 	@Test
 	public final void gatewayParameters() {
 		final GatewayParameterAggregation<Share> gatewayParameterAggregation = shareGatewayParameterService.gatewayParameters(share);
-		
+
 		Assert.assertEquals(share, gatewayParameterAggregation.domain());
 		Assert.assertEquals(1, gatewayParameterAggregation.gatewayParameters().size());
-		Assert.assertEquals(gatewayParameter,  gatewayParameterAggregation.gatewayParameters().stream().findAny().get());
+		Assert.assertEquals(gatewayParameter, gatewayParameterAggregation.gatewayParameters().stream().findAny().get());
 	}
-	
+
 	@Test
 	public final void save() {
 		shareGatewayParameterService.save(gatewayParameter);
-		
+
 		Mockito.verify(gatewayParameterRepository).save(gatewayParameter);
 	}
-	
+
 	@Test
 	public final void dependencies() throws BeanInstantiationException, NoSuchMethodException, SecurityException {
-		
+
 		final Object service = BeanUtils.instantiateClass(shareGatewayParameterService.getClass().getDeclaredConstructor(GatewayParameterRepository.class), gatewayParameterRepository);
-	
-		Assert.assertEquals(gatewayParameterRepository, DataAccessUtils.requiredSingleResult(Arrays.asList(ShareGatewayParameterServiceImpl.class.getDeclaredFields()).stream().filter(field -> dependencies.containsKey(field.getType())).map(field -> ReflectionTestUtils.getField(service, field.getName())).collect(Collectors.toList())));
+
+		Assert.assertEquals(gatewayParameterRepository,
+				DataAccessUtils.requiredSingleResult(Arrays.asList(ShareGatewayParameterServiceImpl.class.getDeclaredFields()).stream().filter(field -> dependencies.containsKey(field.getType())).map(field -> ReflectionTestUtils.getField(service, field.getName())).collect(Collectors.toList())));
 	}
 }
