fc8a2ac042ec5864d4b28a45b4f252be714a7577
Manfred Quasten <manfred.quasten@arcor.de>
2017-11-14 21:10:25 +0100

    extract gateways

--- portfolio.web/src/main/java/de/mq/portfolio/share/support/GatewaysAO.java
+++ portfolio.web/src/main/java/de/mq/portfolio/share/support/GatewaysAO.java
@@ -2,11 +2,18 @@ package de.mq.portfolio.share.support;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import de.mq.portfolio.gateway.Gateway;
 import de.mq.portfolio.gateway.GatewayParameter;
 
 @Component("gateways")
@@ -15,11 +22,35 @@ public class GatewaysAO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	private String code;
-	
+	private String message; 
 	private final List<GatewayParameter> gatewayParameters = new ArrayList<>();
+	private final Map<Gateway,Date> updated = new HashMap<>();
+	public  String getMessage() {
+		return message;
+	}
 
+	
+	void setMessage(String message) {
+		this.message = message;
+	}
+
+
+	public final Date lastUpdate(final Gateway gateway) {
+		return updated.get(gateway);
+	}
+	
+
+	
+	public void assign( Collection<Entry<Gateway,Date>> updates) {
+		 
+		this.updated.putAll(updates.stream().collect(Collectors.toMap(Entry::getKey,Entry::getValue)));
+	}
 	public List<GatewayParameter> getGatewayParameters() {
 		return gatewayParameters;
+	}
+	 void setGatewayParameters( Collection<GatewayParameter> gatewayParameters ) {
+		 this.gatewayParameters.clear();
+		this.gatewayParameters.addAll(gatewayParameters);
 	}
 
 	public String getCode() {
--- portfolio.web/src/main/java/de/mq/portfolio/share/support/GatewaysControllerImpl.java
+++ portfolio.web/src/main/java/de/mq/portfolio/share/support/GatewaysControllerImpl.java
@@ -1,25 +1,70 @@
 package de.mq.portfolio.share.support;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
 
+import de.mq.portfolio.gateway.GatewayParameter;
 import de.mq.portfolio.gateway.ShareGatewayParameterService;
+import de.mq.portfolio.share.ShareService;
 
 @Component("gatewaysController")
 @Scope("singleton")
 public class GatewaysControllerImpl {
 	
 	private final ShareGatewayParameterService shareGatewayParameterService; 
+	private final ShareService shareService; 
+	
+	static final String HTML_EXTENSION = ".html";
+	static final String ERROR_HTML_PATTERN = "<h2>Error during Download %s</h2><h4>%s</h4><label>%s</label>";
+	static final String CONTENT_DISPOSITION_HEADER = "Content-Disposition";
+	static final String FILE_ATTACHEMENT_FORMAT = "attachment; filename=\"%s\"";
 	
 	@Autowired
-	GatewaysControllerImpl(ShareGatewayParameterService shareGatewayParameterService) {
+	GatewaysControllerImpl(ShareGatewayParameterService shareGatewayParameterService, final ShareService shareService) {
 		this.shareGatewayParameterService = shareGatewayParameterService;
+		this.shareService=shareService;
 	}
 
 	public void init(final GatewaysAO gatewaysAO) {
-		System.out.println("*** GatewaysControllerImpl.init() ***");
-		System.out.println(shareGatewayParameterService);
+	
+		shareService.timeCourse(gatewaysAO.getCode()).ifPresent(timecourse -> gatewaysAO.assign(timecourse.updates()));
+		try {
+			gatewaysAO.setGatewayParameters(shareGatewayParameterService.aggregationForAllGateways(new ShareImpl(gatewaysAO.getCode())).gatewayParameters());
+		} catch (final Exception ex) {
+			gatewaysAO.setMessage(ex.getMessage());
+		}
+	}
+	
+	
+	public void download(final FacesContext facesContext, final GatewayParameter gatewayParameter) throws IOException {
+		final ExternalContext externalContext = facesContext.getExternalContext();
+		try {
+			final byte[] content = shareGatewayParameterService.history(gatewayParameter).getBytes();
+			externalContext.responseReset();
+			externalContext.setResponseContentLength(content.length);
+			externalContext.setResponseHeader(CONTENT_DISPOSITION_HEADER, String.format(FILE_ATTACHEMENT_FORMAT, gatewayParameter.gateway().downloadName(gatewayParameter.code())));
+			externalContext.getResponseOutputStream().write(content);
+			facesContext.responseComplete();
+		} catch (final HttpClientErrorException clientErrorException) {
+			final StringWriter errors = new StringWriter();
+			clientErrorException.printStackTrace(new PrintWriter(errors));
+			final byte[] content = String.format(ERROR_HTML_PATTERN, gatewayParameter.gateway(), clientErrorException.getMessage(), errors).getBytes();
+			externalContext.responseReset();
+			externalContext.setResponseContentLength(content.length);
+			externalContext.setResponseHeader(CONTENT_DISPOSITION_HEADER, String.format(FILE_ATTACHEMENT_FORMAT, gatewayParameter.gateway().id(gatewayParameter.code()) + HTML_EXTENSION));
+			externalContext.getResponseOutputStream().write(content);
+			facesContext.responseComplete();
+		}
+
 	}
 
 }
