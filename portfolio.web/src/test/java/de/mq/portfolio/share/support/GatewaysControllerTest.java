d2fc7cdce8798ef5bd85243fa6b43cc0146b439c
Manfred Quasten <manfred.quasten@arcor.de>
2017-11-22 20:33:44 +0100

    GatewaysController coverage

---
+++ portfolio.web/src/test/java/de/mq/portfolio/share/support/GatewaysControllerTest.java
@@ -0,0 +1,101 @@
+package de.mq.portfolio.share.support;
+
+import java.io.IOException;
+import java.util.AbstractMap;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Map.Entry;
+
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
+import java.util.Optional;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+
+import com.lowagie.text.pdf.codec.Base64.OutputStream;
+
+import de.mq.portfolio.gateway.Gateway;
+import de.mq.portfolio.gateway.GatewayParameter;
+import de.mq.portfolio.gateway.GatewayParameterAggregation;
+import de.mq.portfolio.gateway.ShareGatewayParameterService;
+import de.mq.portfolio.share.Share;
+import de.mq.portfolio.share.ShareService;
+import de.mq.portfolio.share.TimeCourse;
+
+
+public class GatewaysControllerTest {
+	
+
+	private static final String HISTORY = "content";
+	private static final String MESSAGE = "don't worry just for test";
+	private static final List<Entry<Gateway, Date>> GATEWAY_ENTRIES = Arrays.asList(new AbstractMap.SimpleImmutableEntry<>(Gateway.GoogleRealtimeRate, new Date()));
+	private static final String CODE = "KO";
+	
+	private final ShareGatewayParameterService shareGatewayParameterService = Mockito.mock(ShareGatewayParameterService.class);
+	private final ShareService shareService = Mockito.mock(ShareService.class);
+	
+	private final   GatewaysControllerImpl gatewaysController = new GatewaysControllerImpl(shareGatewayParameterService, shareService);
+	private final TimeCourse timeCourse = Mockito.mock(TimeCourse.class);
+	private final GatewayParameter gatewayParameter = Mockito.mock(GatewayParameter.class);
+	
+	private final GatewaysAO gatewaysAO = Mockito.mock(GatewaysAO.class);
+	@SuppressWarnings("unchecked")
+	private final GatewayParameterAggregation<Share> gatewayParameterAggregation = Mockito.mock(GatewayParameterAggregation.class);
+	private final ArgumentCaptor<Share> shareCaptor = ArgumentCaptor.forClass(Share.class);
+	
+	private final FacesContext facesContext = Mockito.mock(FacesContext.class);
+	private final ExternalContext externalContext = Mockito.mock(ExternalContext.class);
+	
+	
+			
+	private final OutputStream outputStream = Mockito.mock(OutputStream.class);
+	@Before
+	public final void setup() throws IOException {
+		Mockito.when(gatewaysAO.getCode()).thenReturn(CODE);
+		Mockito.when(shareService.timeCourse(CODE)).thenReturn(Optional.of(timeCourse));
+		Mockito.when(shareGatewayParameterService.aggregationForAllGateways(shareCaptor.capture())).thenReturn(gatewayParameterAggregation);
+		Mockito.when(gatewayParameterAggregation.gatewayParameters()).thenReturn(Arrays.asList(gatewayParameter));
+		Mockito.when(timeCourse.updates()).thenReturn(GATEWAY_ENTRIES);
+		Mockito.when(facesContext.getExternalContext()).thenReturn(externalContext);
+		Mockito.when(shareGatewayParameterService.history(gatewayParameter)).thenReturn(HISTORY);
+		Mockito.when(gatewayParameter.gateway()).thenReturn(Gateway.GoogleRealtimeRate);
+		Mockito.when(externalContext.getResponseOutputStream()).thenReturn(outputStream);
+		Mockito.when(gatewayParameter.code()).thenReturn(CODE);
+		
+	}
+	
+	@Test
+	public final void init() {
+		gatewaysController.init(gatewaysAO);
+		Assert.assertEquals(CODE, shareCaptor.getValue().code());
+		Mockito.verify(gatewaysAO).assign(GATEWAY_ENTRIES);
+		Mockito.verify(gatewaysAO).setGatewayParameters(Arrays.asList(gatewayParameter));
+	}
+	
+	@Test
+	public final void initException() {
+		Mockito.doThrow(new IllegalStateException(MESSAGE)).when(shareGatewayParameterService).aggregationForAllGateways(shareCaptor.capture());
+		gatewaysController.init(gatewaysAO);
+		Mockito.verify(gatewaysAO).setMessage(MESSAGE);
+	}
+	
+	@Test
+	public final void download() throws IOException {
+		gatewaysController.download(facesContext, gatewayParameter);
+		
+		Mockito.verify(externalContext).responseReset();
+		Mockito.verify(externalContext).setResponseContentLength(HISTORY.length());
+		
+		Mockito.verify(outputStream).write(HISTORY.getBytes());
+		Mockito.verify(facesContext).responseComplete();
+		
+		Mockito.verify(externalContext).setResponseHeader(GatewaysControllerImpl.CONTENT_DISPOSITION_HEADER,String.format(GatewaysControllerImpl.FILE_ATTACHEMENT_FORMAT, Gateway.GoogleRealtimeRate.downloadName(CODE)));
+	}
+
+}
